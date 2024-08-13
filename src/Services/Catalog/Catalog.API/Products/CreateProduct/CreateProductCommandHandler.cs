76542c16ef04a9deb1729a8289da5a6c4f114f0b
Yener Yardim <yyardim@gmail.com>
2024-08-13 00:24:03 -0700

    implement Mapster, Carter & Mediatr; add CreateProduct Handler & EndPoint;

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Categories, string Description, string ImageFile, decimal Price) 
+    : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler 
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, 
+        CancellationToken cancellationToken)
+    {
+        Product product = new()
+        {
+            Name = command.Name,
+            Categories = command.Categories,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        // Save product to database
+
+        return new CreateProductResult(Guid.NewGuid());
+    }
+}
